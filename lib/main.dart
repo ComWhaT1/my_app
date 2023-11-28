eb8d33371b781e4dfd3f62bbc6477c27407b2357
ComWhaT <Sahadsawat871@gmail.com>
2023-11-28 11:43:26 +0700

    แยก Widget เป็น Stageless

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/counter_area.dart';
 
 void main() {
   runApp(
     MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.teal,
-        // brightness: Brightness.dark,
-          
+        // brightness: Brightness.dark,         
       ),
       title: "My Home",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Main"),
-        ),
-        body: Text("HELLO"),
-      ),
+      home: MyApp(),
       debugShowCheckedModeBanner: false,
-
-
     ),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HOME'),
+      ),
+      body: CounterArea(),
+    );
+  }
 }
